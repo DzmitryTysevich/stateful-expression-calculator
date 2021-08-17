c1e49bd5fa830d58368c77fa72e4f538692eb736
dzmitrytysevich <dztysevich@gmail.com>
2021-08-17 22:40:41 +0300

    add

---
+++ src/main/webapp/lexemanalyzer/ServletFilter.java
@@ -0,0 +1,23 @@
+package lexemanalyzer;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter
+public class ServletFilter implements javax.servlet.Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
